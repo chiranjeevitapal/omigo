cb43dfdc30d06567f25328d61d200424fdf7b8bd
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2018-05-15 23:24:20 +0530

    fixed Deprecate and Remove URL.createObjectURL for MediaStream

--- client/src/js/chat-client.js
+++ client/src/js/chat-client.js
@@ -5,7 +5,7 @@
  * github: https://github.com/gnuns
  **********************************************/
 'use strict';
-window.chatClient = (function() {
+window.chatClient = (function () {
   const socket = io(window.serverURI);
   let hasPartner = false;
   let isVideoChat = false;
@@ -36,7 +36,9 @@ window.chatClient = (function() {
   }
 
   function sendLocalInfo() {
-    socket.emit('info', {isVideoChat: isVideoChat})
+    socket.emit('info', {
+      isVideoChat: isVideoChat
+    })
   }
 
   function handleServerDisconnection() {
@@ -52,26 +54,29 @@ window.chatClient = (function() {
   }
 
   function tryVideoChat() {
-    navigator.mediaDevices.getUserMedia({video: true, audio: true})
-    .then(function (_localMediaStream) {
-      localMediaStream = _localMediaStream;
-      isVideoChat = true;
-      sendLocalInfo();
-      chatBox.changeChatMode(true);
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+      })
+      .then(function (_localMediaStream) {
+        localMediaStream = _localMediaStream;
+        isVideoChat = true;
+        sendLocalInfo();
+        chatBox.changeChatMode(true);
 
-      let $myVideo = $('<video muted>');
-
-      $('.video>.me').html('');
-      $('.video>.me').append($myVideo);
-      $myVideo.attr('src', window.URL.createObjectURL(localMediaStream));
-      $myVideo[0].play();
-    })
-    .catch(function (err) {
-      localMediaStream = null;
-      isVideoChat = false;
-      sendLocalInfo();
-      chatBox.changeChatMode(false);
-    });
+        var localVideo = document.createElement('video');
+        //localVideo.muted = true;
+        $('.video>.me').html('');
+        $('.video>.me').append(localVideo);
+        localVideo.srcObject = localMediaStream;
+        localVideo.play();
+      })
+      .catch(function (err) {
+        localMediaStream = null;
+        isVideoChat = false;
+        sendLocalInfo();
+        chatBox.changeChatMode(false);
+      });
   }
 
   function handleSysInfo(code) {
@@ -118,31 +123,41 @@ window.chatClient = (function() {
     peer.handleOffer(offer, function (err) {
       if (err) return;
       peer.answer(function (err, answer) {
-          if (!err) socket.emit('videochat_offer_ok', answer);
+        if (!err) socket.emit('videochat_offer_ok', answer);
       });
     });
   }
 
   function setupPeerConnection() {
     peer = new PeerConnection({
-      'iceServers': [{'url': 'stun:stun.l.google.com:19302'},
-                     {'url': 'stun:stun1.l.google.com:19302'},
-                     {'url': 'stun:stun2.l.google.com:19302'},
-                     {'url': 'stun:stun3.l.google.com:19302'}]});
+      'iceServers': [{
+          'url': 'stun:stun.l.google.com:19302'
+        },
+        {
+          'url': 'stun:stun1.l.google.com:19302'
+        },
+        {
+          'url': 'stun:stun2.l.google.com:19302'
+        },
+        {
+          'url': 'stun:stun3.l.google.com:19302'
+        }
+      ]
+    });
 
     peer.addStream(localMediaStream);
     peer.on('addStream', function (event) {
       let remoteMediaStream = event.stream;
       if (partnerIsStreaming) return false;
       partnerIsStreaming = true;
-      let $strangerVideo = $('<video>');
 
+      var strangerVideo = document.createElement('video');
       $('.video>.stranger>video').remove();
-      $('.video>.stranger').append($strangerVideo);
-      $strangerVideo.attr('src', window.URL.createObjectURL(remoteMediaStream));
+      $('.video>.stranger').append(strangerVideo);
+      strangerVideo.srcObject = remoteMediaStream;
       setTimeout(function () {
-        if ($strangerVideo[0].paused) {
-          $strangerVideo[0].play();
+        if (strangerVideo[0].paused) {
+          strangerVideo.play();
         }
       }, 200);
     });
